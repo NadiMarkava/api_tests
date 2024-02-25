9ac00d2a7b76b3da61d2042d6bb24d9026ef69ca
nknysh <nadimarkava@gmail.com>
2024-02-25 15:38:41 +0300

    refactored code

--- src/test/java/GraphqlTests.java
+++ src/test/java/GraphqlTests.java
@@ -2,7 +2,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -20,61 +19,44 @@ import static org.testng.Assert.assertTrue;
 
 public class GraphqlTests {
 
-    private static final String usersUrl = "https://gorest.co.in/public/v2/graphql";
+    private static final String URL = "https://gorest.co.in/public/v2/graphql";
+    private static final String BASIC_PATH = "src/test/resources/api/graphql/";
+    private static final String TOKEN = "493ea62f49818ae1b6bd35cb27bdf8e20e73fb75e1d37a86117f3a0305364a2f";
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final String TOKEN = "";
 
     @Test
     public void getUsers() throws IOException, InterruptedException, URISyntaxException {
-        String getPath = "src/test/resources/api/graphql/_get/rq.json";
-        HttpRequest request = buildRequest(getPath);
-        HttpResponse<String> response = buildResponse(request);
-        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
-        String r = response.body();
-        List<Map<String, Object>>userList = JsonPath.from(response.body()).getList("data.users.nodes");
-        for (Map<String, Object> map : userList) {
-                SoftAssert softAssert = new SoftAssert();
-                softAssert.assertTrue(map.get("id").toString().matches("^\\d{7}$"), "Id does not match");
-                softAssert.assertTrue(map.get("name").toString().matches("[a-zA-Z]+(\\.)?\\s[a-zA-Z]+(\\.)?(\\s[a-zA-Z]+(\\.)?)?+(\\s[a-zA-Z])?"), "Name does not match");
-                softAssert.assertTrue(map.get("email").toString().matches("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,9}$"), "Email does not match");
-                softAssert.assertTrue(map.get("gender").toString().matches("(?:male|female)$"), "Gender does not match");
-                softAssert.assertTrue(map.get("status").toString().matches("(?:inactive|active)$"), "Status does not match");
-                softAssert.assertAll();
-            }
+        String rq = Files.readString(Path.of(BASIC_PATH + "_get/rq.json"));
+        HttpRequest request = sendRequest(rq);
+        HttpResponse<String> response = getResponse(request);
+        List<Map<String, Object>> userList = JsonPath.from(response.body()).getList("data.users.nodes");
+        for (Map<String, Object> userData : userList) {
+            verifyValidData(userData);
+        }
     }
 
     @Test
     public void getUserById() throws IOException, InterruptedException, URISyntaxException {
-            String getByIdPath = "src/test/resources/api/graphql/_get_by_id/rq.json";
-            String requestTmp = Files.readString(Path.of(getByIdPath));
-            String id = "5913607";
-            String rq = String.format(requestTmp, id);
-            HttpRequest request = buildRequest(rq);
-            HttpResponse<String> response = buildResponse(request);
-            assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
-            Map<String, Object> userParams = JsonPath.from(response.body()).getMap("data.user");
-            SoftAssert softAssert = new SoftAssert();
-            int responseId = (int) userParams.get("id");
-            softAssert.assertTrue(responseId == Integer.valueOf(id), "Id does not match");
-            softAssert.assertTrue(userParams.get("name").toString().matches("[a-zA-Z]+(\\.)?\\s[a-zA-Z]+(\\.)?(\\s[a-zA-Z]+(\\.)?)?+(\\s[a-zA-Z])?"), "Name does not match");
-            softAssert.assertTrue(userParams.get("email").toString().matches("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,9}$"), "Email does not match");
-            softAssert.assertTrue(userParams.get("gender").toString().matches("(?:male|female)$"), "Gender does not match");
-            softAssert.assertTrue(userParams.get("status").toString().matches("(?:inactive|active)$"), "Status does not match");
-            softAssert.assertAll();
+        String getByIdPath = BASIC_PATH + "_get_by_id/rq.json";
+        String requestTmp = Files.readString(Path.of(getByIdPath));
+        String id = "5850514";
+        String rq = String.format(requestTmp, id);
+        HttpRequest request = sendRequest(rq);
+        HttpResponse<String> response = getResponse(request);
+        Map<String, Object> userData = JsonPath.from(response.body()).getMap("data.user");
+        verifyValidData(userData);
     }
 
     @Test
     public void postUser() throws IOException, InterruptedException, URISyntaxException {
-        String postPath = "src/test/resources/api/graphql/_post/rq.json";
         String name = "Tomas";
         String email = "tom@kruz.example";
         String gender = "male";
         String status = "active";
+        String postPath = BASIC_PATH + "_post/rq.json";
         String rq = getPath(postPath, name, email, gender, status);
-        HttpRequest request = buildRequest(rq);
-        HttpResponse<String> response = buildResponse(request);
-        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
-        String rs = response.body();
+        HttpRequest request = sendRequest(rq);
+        HttpResponse<String> response = getResponse(request);
         String idFromResponse = JsonPath.from(response.body()).getString("data.createUser.user.id");
         int id = Integer.valueOf(idFromResponse);
         User requestUser = new User(id, name, email, gender, status);
@@ -90,25 +72,12 @@ public class GraphqlTests {
 
     @Test
     public void putUser() throws IOException, InterruptedException, URISyntaxException {
-        String postPath = "src/test/resources/api/graphql/_post/rq.json";
-        String putPath = "src/test/resources/api/graphql/_put/rq.json";
-        String name = "Andreas";
-        String email = "tom@kruz.example";
-        String gender = "male";
-        String status = "active";
-        String postRq = String.format(postPath, name, email, gender, status);
-        HttpRequest request = buildRequest(postRq);
-        HttpResponse<String> response = buildResponse(request);
-        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
-        String idFromResponse = JsonPath.from(response.body()).getString("data.createUser.user.id");
-        String nameToUpdate = "Sara";
-        String emailToUpdate = "sara@hermas.donut";
-        String genderToUpdate = "female";
-        String statusToUpdate = "inactive";
-        String putRq = String.format(putPath, idFromResponse, nameToUpdate, emailToUpdate, genderToUpdate, statusToUpdate);
-        request = buildRequest(putRq);
-        response = buildResponse(request);
-        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
+        String putPath = BASIC_PATH + "_put/rq.json";
+        String userId = postUser("Andreas", "tom@kruz.example", "male", "active");
+        String requestTmp = Files.readString(Path.of(putPath));
+        String putRq = String.format(requestTmp, userId, "Sara", "sara@hermas.donut", "female", "inactive");
+        HttpRequest request = sendRequest(putRq);
+        HttpResponse<String> response = getResponse(request);
         String rs = response.body();
         String rsPath = "src/test/resources/api/graphql/_put/rs.json";
         String expectedRs = Files.readString(Path.of(rsPath));
@@ -117,35 +86,29 @@ public class GraphqlTests {
 
     @Test
     public void deleteUser() throws IOException, InterruptedException, URISyntaxException {
-        String deletePath = "src/test/resources/api/graphql/_delete/rq.json";
-        String pathTmp = "src/test/resources/api/graphql/_post/rq.json";
-        String postRq = getPath(pathTmp, "Rayn", "rayn@red.example", "male", "inactive");
-        HttpRequest request = buildRequest(postRq);
-        HttpResponse<String> response = buildResponse(request);
-        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
-        String idFromResponse = JsonPath.from(response.body()).getString("data.createUser.user.id");
+        String deletePath = BASIC_PATH + "_delete/rq.json";
+        String userId = postUser("Rayn", "rayn@red.example", "male", "inactive");
         String requestTmp = Files.readString(Path.of(deletePath));
-        String rq = String.format(requestTmp, idFromResponse);
-        request = buildRequest(rq);
-        response = buildResponse(request);
-        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
-        String rs = response.body();
-        String rsPath = "src/test/resources/api/graphql/_delete/rs.json";
+        String rq = String.format(requestTmp, userId);
+        HttpRequest request = sendRequest(rq);
+        HttpResponse<String> response = getResponse(request);
+        String rsPath = BASIC_PATH + "_delete/rs.json";
         String expectedRs = Files.readString(Path.of(rsPath));
-        assertEquals(expectedRs, rs, "Responses are not equal");
+        assertEquals(expectedRs, response.body(), "Responses are not equal");
     }
 
-    public HttpResponse<String> buildResponse(HttpRequest request) throws IOException, InterruptedException {
+    public HttpResponse<String> getResponse(HttpRequest request) throws IOException, InterruptedException {
         HttpResponse<String> response = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.NORMAL)
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
         return response;
     }
 
-    public HttpRequest buildRequest(String path) throws URISyntaxException, IOException, InterruptedException {
+    public HttpRequest sendRequest(String path) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(usersUrl))
+                .uri(new URI(URL))
                 .header("Authorization", "Bearer " + TOKEN)
                 .version(HttpClient.Version.HTTP_2)
                 .headers("Content-Type", "application/json")
@@ -153,9 +116,30 @@ public class GraphqlTests {
                 .build();
         return request;
     }
+
     public String getPath(String pathTmp, String name, String email, String gender, String status) throws IOException {
         String requestTmp = Files.readString(Path.of(pathTmp));
         String path = String.format(requestTmp, name, email, gender, status);
         return path;
+    }
+
+    public String postUser(String name, String email, String gender, String status) throws IOException, URISyntaxException, InterruptedException {
+        String pathTmp = BASIC_PATH + "_post/rq.json";
+        String postRq = getPath(pathTmp, name, email, gender, status);
+        HttpRequest request = sendRequest(postRq);
+        HttpResponse<String> response = getResponse(request);
+        assertEquals(response.statusCode(), HttpURLConnection.HTTP_OK);
+        String userId = JsonPath.from(response.body()).getString("data.createUser.user.id");
+        return userId;
+    }
+
+    public void verifyValidData(Map<String, Object> user) {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(user.get("id").toString().matches("^\\d{7}$"), "Id does not match");
+        softAssert.assertTrue(user.get("name").toString().matches("[a-zA-Z]+(\\.)?\\s[a-zA-Z]+(\\.)?(\\s[a-zA-Z]+(\\.)?)?+(\\s[a-zA-Z])?"), "Name does not match");
+        softAssert.assertTrue(user.get("email").toString().matches("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,9}$"), "Email does not match");
+        softAssert.assertTrue(user.get("gender").toString().matches("(?:male|female)$"), "Gender does not match");
+        softAssert.assertTrue(user.get("status").toString().matches("(?:inactive|active)$"), "Status does not match");
+        softAssert.assertAll();
     }
 }
